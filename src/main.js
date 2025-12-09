cd446a1989379582a23d695dee69221fec252bd3
Naokuma005288 <kunamaokunamao2828@gmail.com>
2025-12-10 00:04:14 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+// src/main.js
+import { setupClock } from "./core/clock.js";
+import { setupWidgetPanel } from "./core/widgetManager.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  setupClock();
+  setupWidgetPanel();
+});
